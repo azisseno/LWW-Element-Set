ee48c4bea1b80f9e94c6ae18e0b3f428a70347ed
Azis Senoaji <venazp@sg.uob>
2020-01-15 22:17:12 +0700

    Initial Commit

---
+++ LLWElementSet/LLWElementSet/LLWElementSet.h
@@ -0,0 +1,19 @@
+//
+//  LLWElementSet.h
+//  LLWElementSet
+//
+//  Created by admin on 15/01/20.
+//  Copyright © 2020 siza. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for LLWElementSet.
+FOUNDATION_EXPORT double LLWElementSetVersionNumber;
+
+//! Project version string for LLWElementSet.
+FOUNDATION_EXPORT const unsigned char LLWElementSetVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <LLWElementSet/PublicHeader.h>
+
+
